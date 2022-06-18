62f6c9036175b0ee8cfa4363bf241644431f06e5
srma4tech <srmag1295@gmail.com>
2022-06-18 20:46:10 +0530

    dynamic allocation, release memory from heap

--- Dynamic Allocation/memory_allocation.cpp
+++ Dynamic Allocation/memory_allocation.cpp
@@ -1,6 +1,39 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    
+int main()
+{
+    int *p = new int;
+    *p = 10;
+    cout << *p << endl;
+
+    double *pd = new double;
+    char *c = new char;
+    int *pa = new int[50];
+
+    cout << "Enter no of elements " << endl;
+    int n;
+    cin >> n;
+
+    int *pa2 = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> pa2[i];
+    }
+
+    int max = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (pa2[i] > max)
+        {
+            max = pa2[i];
+        }
+    }
+    cout << "Maximum Element is " << max << endl;
+    delete p;
+    delete pd;
+    delete c;
+    delete pa;
+    delete[] pa2;
 }
