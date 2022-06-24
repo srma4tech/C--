1f8f3b508a24c723a5742f076333bd541e14da84
srma4tech <srmag1295@gmail.com>
2022-06-25 02:34:22 +0530

    test

--- TestingCPP/Test.cpp
+++ TestingCPP/Test.cpp
@@ -11,7 +11,6 @@ using namespace std;
 //     cout << ans;
 // }
 
-
 // void print(int n){
 //     if(n < 0){
 //         return;
@@ -42,3 +41,13 @@ using namespace std;
 //     print(num);
 // }
 
+// int main()
+// {
+//     int i = 0, x = 0;
+//     for (i = 1; i < 10; i *= 2)
+//     {
+//         x++;
+//         cout << x;
+//     }
+//     cout << x;
+// }
