ae98be2a24facd46b1476fdc699559e2d68d9625
srma4tech <srmag1295@gmail.com>
2022-07-05 17:45:30 +0530

    commit  update

---
+++ Recursion - 1/FirstIndex.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int fi(int arr[], int n, int x)
+{
+    if (n < 1)
+    {
+        return 0;
+    }
+    else if (arr[0] == x)
+    {
+        return 0;
+    }
+    else
+    {
+        int i = fi(arr + 1, n - 1, x);
+        return i == -1 ? i : i + 1;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int x;
+    cout << "Enter Element to Search for first Index";
+    cin >> x;
+
+    cout << "First Index is : " << fi(arr, n, x) << endl;
+}
