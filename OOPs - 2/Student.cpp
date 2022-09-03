864a9c0966ba2bf2fe50eebcb1dc83b90389ff79
srma4tech <srmag1295@gmail.com>
2022-09-03 18:39:23 +0530

    Deep and shallow copy

---
+++ OOPs - 2/Student.cpp
@@ -0,0 +1,41 @@
+// Shallow and deep copy
+
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+class Student
+{
+    int age;
+    char *name;
+
+public:
+    Student(int age, char *name)
+    {
+        this->age = age;
+        // shallow copy - should be avoided
+        // this->name = name;
+
+        // Deep Copy
+        this->name = new char[strlen(name) + 1];
+        strcpy(this->name, name);
+    }
+
+    void display()
+    {
+        cout << name << " & " << age<<endl;
+    }
+};
+
+int main()
+{
+    char name[] = "Sachin";
+    Student s1(28, name);
+    s1.display();
+
+    name[3] = 'e';
+    Student s2(24, name);
+    s2.display();
+
+    s1.display();
+}
