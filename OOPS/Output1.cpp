732c2e58c60fd3cf144ef9b01ad7c1a8d9df8b77
srma4tech <srmag1295@gmail.com>
2022-09-05 23:15:20 +0530

    update

--- OOPS/Output1.cpp
+++ OOPS/Output1.cpp
@@ -125,3 +125,85 @@ using namespace std;
 //     s1.x = age;
 //     cout << s1.rollNumber << " " << s1.age << " " << s1.x;
 // }
+
+// class Student {
+//     public :
+
+//     int rollNumber;
+//     int age;
+
+// };
+
+
+// int main() {
+//     Student s1;
+
+//     Student const s2 = s1;
+
+//     s1.rollNumber = 101;
+//     s1.age = 20;
+
+//     cout << s2.rollNumber << " " << s2.age;
+// }
+
+
+// class Student {
+//     int rollNumber;
+
+//     public :
+
+//     int age;
+
+//     Student(int r) {
+//         rollNumber = r;
+//     }
+
+//     int getRollNumber() {
+//         return rollNumber;
+//     }
+
+// };
+
+
+// int main() {
+//     Student s1(101);
+//     s1.age = 20;
+
+//     Student const s2 = s1;
+//     cout << s2.getRollNumber();
+// }
+
+// class Student {
+//     public :
+
+//     int rollNumber;
+
+//     static int getRollNumber() {
+//         return rollNumber;
+//     }
+// };
+
+
+// int main() {
+//     Student s;
+//     s.rollNumber = 101;
+//     cout << s.getRollNumber() << endl;
+// }
+
+class Student {
+    public :
+
+    int rollNumber;
+    static int totalStudents;
+
+    Student() {
+        totalStudents++;
+    }
+};
+
+int Student::totalStudents = 20;
+
+int main() {
+    Student s1, s2, s3, s4;
+    cout << Student :: totalStudents;
+}
