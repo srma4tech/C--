cd098c4c7ab8a9ad9b8768a288c1c7cdc05a1baa
srma4tech <srmag1295@gmail.com>
2022-09-06 23:50:05 +0530

    takeInput() implemented

--- LinkedList - 1/LinkedList.cpp
+++ LinkedList - 1/LinkedList.cpp
@@ -2,6 +2,31 @@
 #include "Node.cpp"
 using namespace std;
 
+Node *takeInput()
+{
+    int data;
+    cin >> data;
+    Node *head = NULL;
+    while (data != -1)
+    {
+        Node *newNode = new Node(data);
+        if (head == NULL)
+        {
+            head = newNode;
+        }
+        else{
+            Node *temp = head;
+            while(temp -> next != NULL){
+                temp = temp->next;
+            }
+            temp ->next = newNode;
+        }
+
+        cin >> data;
+    }
+    return head;
+}
+
 void print(Node *head)
 {
     Node *temp = head;
@@ -10,24 +35,26 @@ void print(Node *head)
         cout << temp->data << " ";
         temp = temp->next;
     }
-    //cout<<head->data;
 }
 
 int main()
 {
+    Node *head = takeInput();
+    
+
     // statically creation
-    Node n1(1);
-    Node *head = &n1;
-    Node n2(2);
+    // Node n1(1);
+    // Node *head = &n1;
+    // Node n2(2);
 
-    Node n3(3);
-    Node n4(4);
-    Node n5(5);
+    // Node n3(3);
+    // Node n4(4);
+    // Node n5(5);
 
-    n1.next = &n2;
-    n2.next = &n3;
-    n3.next = &n4;
-    n4.next = &n5;
+    // n1.next = &n2;
+    // n2.next = &n3;
+    // n3.next = &n4;
+    // n4.next = &n5;
 
     print(head);
 
