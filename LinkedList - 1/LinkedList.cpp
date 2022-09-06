09b58156680ff7f2900c92c06227a0d1899ca7cc
srma4tech <srmag1295@gmail.com>
2022-09-07 01:56:26 +0530

    Linked List length, insert

--- LinkedList - 1/LinkedList.cpp
+++ LinkedList - 1/LinkedList.cpp
@@ -7,19 +7,19 @@ Node *takeInput()
     int data;
     cin >> data;
     Node *head = NULL;
+    Node *tail = NULL;
     while (data != -1)
     {
         Node *newNode = new Node(data);
         if (head == NULL)
         {
             head = newNode;
+            tail = newNode;
         }
-        else{
-            Node *temp = head;
-            while(temp -> next != NULL){
-                temp = temp->next;
-            }
-            temp ->next = newNode;
+        else
+        {
+            tail->next = newNode;
+            tail = tail->next;
         }
 
         cin >> data;
@@ -37,10 +37,69 @@ void print(Node *head)
     }
 }
 
+int length(Node *head)
+{
+    // Write your code here
+    int count = 0;
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+void printIthNode(Node *head, int i)
+{
+    int len = length(head);
+    int ele = 0;
+    Node *temp = head;
+    for (int itr = 0; itr < len; itr++)
+    {
+        temp = temp->next;
+        if (itr == i - 1)
+        {
+            cout << temp->data;
+        }
+    }
+}
+
+void insertNode(Node *head, int i, int data)
+{
+    Node *newNode = new Node(data);
+    Node *temp = head;
+    int count = 0;
+    while (count < i - 1)
+    {
+        temp = temp->next;
+        count++;
+    }
+    if (temp != NULL)
+    {
+        Node *a = temp->next;
+        temp->next = newNode;
+        newNode->next = a;
+    }
+}
+
 int main()
 {
     Node *head = takeInput();
-    
+    int pos, ele;
+    cout << "Enter position of element : " << endl;
+    cin >> pos;
+    cout << "Linked List is : ";
+    print(head);
+    cout << endl;
+    cout << "Length of Linked List is : " << length(head) << endl;
+    cout << "Element on " << pos << " Linked List is : ";
+    printIthNode(head, pos);
+    cout << "Enter element which you want to insert : " << endl;
+    cin >> ele;
+    insertNode(head, pos, ele);
+    cout << "Updated Linked List is : ";
+    print(head);
 
     // statically creation
     // Node n1(1);
@@ -55,8 +114,6 @@ int main()
     // n2.next = &n3;
     // n3.next = &n4;
     // n4.next = &n5;
-
-    print(head);
 
     // n1.next = &n2;
 
