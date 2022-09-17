45993ab0de2fd68cb905ba9384ed59e1d412b62f
srma4tech <srmag1295@gmail.com>
2022-09-17 13:44:01 +0530

    add practice questions functions

--- LinkedList - 2/Functions.cpp
+++ LinkedList - 2/Functions.cpp
@@ -247,6 +247,200 @@ Node *evenAfterOdd(Node *head)
     }
 }
 
+Node *skipMdeleteN(Node *head, int M, int N)
+{
+    if (head == NULL)
+        return NULL;
+
+    if (M == 0)
+    {
+        return 0;
+    }
+    Node *pt = head;
+    for (int i = 1; i < M; i++)
+    {
+        if (pt->next == NULL)
+            break;
+        pt = pt->next;
+    }
+
+    for (int i = 1; i <= N; i++)
+    {
+        if (pt->next == NULL)
+            break;
+        Node *t = pt->next;
+        pt->next = t->next;
+        t->next = NULL;
+        delete t;
+    }
+
+    Node *ans = skipMdeleteN(pt->next, M, N);
+    return head;
+}
+
+Node *swapNodes(Node *head, int i, int j)
+{
+    if (i > j)
+    {
+        i = i + j;
+        j = i - j;
+        i = i - j;
+    }
+    // Find nodes
+    if (i == 0)
+    {
+        if (j == 1)
+        {
+            Node *cur = head->next;
+            head->next = cur->next;
+            cur->next = head;
+            return cur;
+        }
+        Node *pt = head;
+        for (int c = 1; c < j; c++)
+            pt = pt->next;
+
+        Node *temp = head->next;
+        Node *cur = pt->next;
+
+        head->next = cur->next;
+        pt->next = head;
+        cur->next = temp;
+        return cur;
+    }
+    if (j - i == 1)
+    {
+        Node *pt = head;
+        for (int c = 1; c < i; c++)
+            pt = pt->next;
+
+        Node *cur = pt->next;
+        Node *cur1 = cur->next;
+
+        cur->next = cur1->next;
+        pt->next = cur1;
+        cur1->next = cur;
+        return head;
+    }
+    Node *pt1 = head;
+    Node *pt2 = head;
+
+    for (int c = 1; c < i; c++)
+        pt1 = pt1->next;
+
+    for (int c = 1; c < j; c++)
+        pt2 = pt2->next;
+
+    Node *cur1 = pt1->next;
+    Node *cur2 = pt2->next;
+
+    Node *temp = cur1->next;
+
+    cur1->next = cur2->next;
+    pt2->next = cur1;
+    pt1->next = cur2;
+    cur2->next = temp;
+
+    return head;
+}
+
+Node *revelist(Node *head)
+{
+
+    if (head == NULL)
+        return NULL;
+    if (head->next == NULL)
+        return head;
+
+    Node *prev = head;
+    Node *cur = head->next;
+    prev->next = NULL;
+    while (cur)
+    {
+        Node *temp = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = temp;
+    }
+
+    return prev;
+}
+
+int getLenght(Node *head)
+{
+    int c = 0;
+    while (head)
+    {
+        head = head->next;
+        c++;
+    }
+    return c;
+}
+
+Node *kReverse(Node *head, int k)
+{
+    if (head == NULL)
+        return NULL;
+    int count = 0;
+
+    Node *front = head;
+    Node *tail = head;
+    Node *newHead;
+
+    for (int i = 1; i < k; i++)
+    {
+        if (tail->next == NULL)
+            break;
+        else
+            tail = tail->next;
+    }
+    Node *newtail = front;
+    Node *temp = tail->next;
+    tail->next = NULL;
+    newHead = revelist(front);
+
+    Node *ans = kReverse(temp, k);
+    newtail->next = ans;
+    return newHead;
+}
+
+Node *bubbleSort(Node *head)
+{
+    Node *i = head;
+
+    while (i)
+    {
+        Node *j = head;
+        Node *prev = head;
+        while (j->next)
+        {
+            Node *temp = j->next;
+            if (j->data > temp->data)
+            {
+                if (j == head)
+                {
+                    j->next = temp->next;
+                    temp->next = j;
+                    prev = temp;
+                    head = prev;
+                }
+                else
+                {
+                    j->next = temp->next;
+                    temp->next = j;
+                    prev->next = temp;
+                    prev = temp;
+                }
+                continue;
+            }
+            prev = j;
+            j = j->next;
+        }
+        i = i->next;
+    }
+    return head;
+}
+
 int main()
 {
     Node *rev = NULL;
