ad5bf7b14e78135629cfcbe3e4cb212f2f0f63e7
srma4tech <srmag1295@gmail.com>
2022-09-18 17:40:56 +0530

    update

---
+++ Stack & Queues/StackUsingDynamicArray.cpp
@@ -0,0 +1,86 @@
+#include<climits>
+#include <iostream>
+using namespace std;
+
+class StackUsingArray
+{
+    int *data;
+    int nextIndex;
+    int capacity;
+
+public:
+    StackUsingArray()
+    {
+        data = new int[];
+        nextIndex = 0;
+        capacity = 4;
+    }
+
+    // return the number of elements present in my stack
+    int size()
+    {
+        return nextIndex;
+    }
+
+    bool isEmpty()
+    {
+        return nextIndex == 0;
+    }
+
+    // insert element in stack
+    void push(int element)
+    {
+        if (nextIndex == capacity)
+        {
+            int *newData = new int[2 * capacity];
+        }
+        data[nextIndex] = element;
+        nextIndex++;
+    }
+
+    // delete element
+    int pop()
+    {
+        if (isEmpty())
+        {
+            cout << "No Elements available" << endl;
+            return INT_MIN;
+        }
+        nextIndex--;
+        return data[nextIndex];
+    }
+    int top()
+    {
+        if (isEmpty())
+        {
+            cout << "No Elements available" << endl;
+            return INT_MIN;
+        }
+        return data[nextIndex - 1];
+    }
+    void display(){
+
+    }
+};
+
+int main() {
+    StackUsingArray s(5);
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+    s.push(50);
+
+    cout<< s.top() <<endl;
+
+    cout<<s.pop()<<endl;
+    cout<<s.pop()<<endl;
+    cout<<s.pop()<<endl;
+    cout<<s.pop()<<endl;
+
+    cout<<s.size()<<endl;
+
+    cout<<s.isEmpty()<<endl;
+
+    return 0;
+}
