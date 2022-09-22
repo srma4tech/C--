99fbd424b31c746bb867657e6cda26ff95f6ad58
srma4tech <srmag1295@gmail.com>
2022-09-23 02:28:34 +0530

    tree

---
+++ Tree/VectorUse.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+
+    cout<<v[0];
+}
