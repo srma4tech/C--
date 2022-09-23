0f99f9848a6b379bc34d65d3b33b444bec88f334
srma4tech <srmag1295@gmail.com>
2022-09-24 01:35:14 +0530

    Tree implementation

---
+++ Tree/TreeUse.cpp
@@ -0,0 +1,25 @@
+#include "TreeNode.h"
+#include <vector>
+#include <iostream>
+using namespace std;
+
+void printTree(TreeNode<int> *root)
+{
+    cout << root->data << endl;
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        printTree(root->children[i]);
+    }
+}
+
+int main()
+{
+    TreeNode<int> *root = new TreeNode<int>(1);
+    TreeNode<int> *node1 = new TreeNode<int>(2);
+    TreeNode<int> *node2 = new TreeNode<int>(3);
+
+    root->children.push_back(node1);
+    root->children.push_back(node2);
+
+    printTree(root);
+}
