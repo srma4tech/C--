99d37a3eb9b491343a03a06fe34c29a18bf259f5
srma4tech <srmag1295@gmail.com>
2022-09-24 01:58:13 +0530

    take input and print tree

--- Tree/TreeUse.cpp
+++ Tree/TreeUse.cpp
@@ -5,21 +5,45 @@ using namespace std;
 
 void printTree(TreeNode<int> *root)
 {
-    cout << root->data << endl;
+    if(root == NULL){
+        return;
+    }
+    cout << root->data << ":";
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        cout << root->children[i]->data << ",";
+    }
+    cout << endl;
     for (int i = 0; i < root->children.size(); i++)
     {
         printTree(root->children[i]);
     }
 }
 
+TreeNode<int> *takeInput() {
+    int rootData;
+    cout<<"Enter data : "<<endl;
+    cin>>rootData;
+    TreeNode<int> *root = new TreeNode<int>(rootData);
+
+    int n;
+    cout<<"Enter numnber of children : " << rootData <<endl;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        TreeNode<int>* child = takeInput();
+        root ->children.push_back(child);
+    }
+    return root;
+}
+
 int main()
 {
-    TreeNode<int> *root = new TreeNode<int>(1);
-    TreeNode<int> *node1 = new TreeNode<int>(2);
-    TreeNode<int> *node2 = new TreeNode<int>(3);
+    // TreeNode<int> *root = new TreeNode<int>(1);
+    // TreeNode<int> *node1 = new TreeNode<int>(2);
+    // TreeNode<int> *node2 = new TreeNode<int>(3);
 
-    root->children.push_back(node1);
-    root->children.push_back(node2);
-
+    // root->children.push_back(node1);
+    // root->children.push_back(node2);
+    TreeNode<int>*root = takeInput();
     printTree(root);
 }
